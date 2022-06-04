2f7304bc7b9e38deb8cd2c094f8cff18cf220ce4
Hein-Pieter van Braam <hp@tmm.cx>
2022-06-04 11:20:42 +0200

    Fix some security issues

--- z-push/entrypoint.sh
+++ z-push/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/root/php_configfile.sh KC_ZPUSH_COMMON_ /etc/zpush/z-push.conf.php
-/root/php_configfile.sh KC_ZPUSH_KOPANO_ /etc/zpush/kopano.conf.php
-/root/php_configfile.sh KC_ZPUSH_AUTODISCOVER_ /etc/zpush/autodiscover.conf.php
+/root/php_configfile.sh Z_PUSH_COMMON_ /etc/z-push/z-push.conf.php
+/root/php_configfile.sh Z_PUSH_KOPANO_ /etc/z-push/kopano.conf.php
+/root/php_configfile.sh Z_PUSH_AUTODISCOVER_ /etc/z-push/autodiscover.conf.php
 
 chown -R kopano.kopano /var/log/z-push
 
